101ca626cd70942ed87a1b12b298f446d3a17350
Dulan Dias <dulansdias@gmail.com>
2025-04-11 19:26:52 +0100

    remove unused permission checking function

--- app/core/permissions.py
+++
@@ -1,6 +0,0 @@
-def has_access(user_id, doc, action):
-    acl = doc.get("access", {})
-    for role, users in acl.items():
-        if user_id in users and action in ["read", "edit", "delete"]:
-            return True
-    return False
