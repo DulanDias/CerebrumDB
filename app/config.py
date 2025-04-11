ec66b13fd7c46a12dbb2ec4be4ccb695041bcb0e
Dulan Dias <dulansdias@gmail.com>
2025-04-11 18:38:32 +0100

    redis and bcrypt issue fix

--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ TRANSFORMER_MODEL = os.getenv("TRANSFORMER_MODEL", "all-mpnet-base-v2")
 CONTINUAL_LEARNING = os.getenv("CONTINUAL_LEARNING", "false") == "true"
 DATA_STORE_TYPE = os.getenv("DATA_STORE_TYPE", "json")
 VECTOR_INDEX_PATH = os.getenv("VECTOR_INDEX_PATH", "vector_index/index.faiss")
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
